d2146eefd731fddcaf6224f833446c1c84f52129
Elva Fan <fan.elva@gmail.com>
2016-03-22 07:48:57 -1000

    can filter posts now!

--- final-project/js/app.js
+++ final-project/js/app.js
@@ -1,39 +1,35 @@
 $( document ).ready(function() {
 	
-// Isotope filtering by div class
-	$('#most-recent a').click(function (e) {
-  	e.preventDefault();
-  	$(this).tab('show'); 
-  	$grid.isotope({ filter: '*' });
-  		});
+	// if user loads URL with hash to begin with, filter content and select right tab
+	if(location.hash.length){
+		filter_target(location.hash.slice(1));
+		$('a[href="'+location.hash+'"]').tab('show');
+		// 'a[href=XXX]' is selecting all a's whose href=XXX
+	}
 
-    $('#popular a').click(function (e) {
-  	e.preventDefault();
-  	$(this).tab('show'); 
-  	$grid.isotope({ filter: '.popular' });
-  		});
+	// define function that filters which posts get displayed
+	function filter_target(target_category){
+		console.log(target_category)
+		// make everything display: appear
+		if(target_category==='most-recent'){
+			$('.post').css('display', 'inline');
+		}
+		else {
+			$('.post.'+target_category).css('display', 'inline');
+			// make posts that doesn't have target category disappear
+			$('.post').not('.'+target_category).css('display','none'); 
+		}
+		console.log($('.post.'+target_category))
+	};
 
-    $('#growth a').click(function (e) {
-  	e.preventDefault();
-  	$(this).tab('show'); 
-  	$grid.isotope({ filter: '.growth' });
-  		});
-
-    $('#leadership a').click(function (e) {
-  	e.preventDefault();
-  	$(this).tab('show'); 
-  	$grid.isotope({ filter: '.leadership' });
-  		});
-
-    $('#personal a').click(function (e) {
-  	e.preventDefault();
-  	$(this).tab('show'); 
-  	$grid.isotope({ filter: '.personal' });
-  		});
-
+	// listen for hash change event - i.e. when url changes to add #tab
+	$(window).on("hashchange",function(e){
+		console.log(location.hash); 
+		filter_target(location.hash.slice(1)); //pass class name to filter_target
+	})
 
 
 
 
     console.log( "ready!" );
-});
+}); //closing ready function
